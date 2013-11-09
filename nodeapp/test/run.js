f8c01881b6261071fa507bf56956257ae6b758d5
bihe <henrik@binggl.net>
2013-11-09 21:08:09 +0100

    Update tests - use mocha-runner to work an mac & win

---
+++ nodeapp/test/run.js
@@ -0,0 +1,23 @@
+var Runner = require ("mocha-runner");
+
+new Runner ({
+    //If the reporter is not specified "dot" is used by default
+    reporter: "spec",
+
+    //Before running the tests all the files are removed to ensure a consistent state except:
+    //- Excluded files
+    //- Test files
+    //- node_modules directory
+    //- This file (the file that uses mocha-runner to run the tests)
+    //It's recommended to create a folder named `tests` and put there all the testing stuff including this file
+    //exclude: ["file1", "file2"],
+
+    //To remove some specific files
+    //If this property is set, "exclude" is ignored
+    //include: ["file1", "file2"],
+
+    tests: ["test-master", "test-sqlite"]
+}).run (function (error){
+    //It's not the Mocha stderr
+    if (error) console.log (error);
+});
--- nodeapp/test/test-sqlite.js
+++ nodeapp/test/test-sqlite.js
@@ -1,25 +1,60 @@
+// test the basic features of sqlite
+
 'use strict';
 
+var sqlite3 = require('sqlite3');
 var should = require('should');
 var assert = require('assert');
 
-describe('SQLite', function(){
+describe('sqlite', function() {
 
 	describe('Basic', function() {
-  
-	  it('should correctly import sqlite3', function(){
-	    var db = require('sqlite3');
-	    db.should.not.eql(undefined);
-	  });
 
-	  it('should create a database connection', function(){
-	    var sqlite3 = require('sqlite3').verbose();
-			var db = new sqlite3.Database(':memory:');
+		var db;
+		before(function(done) {
+			db = new sqlite3.Database(':memory:', done);
+		});
 
-			db.should.not.eql(undefined);
+		var inserted = 0;
+		var retrieved = 0;
 
-			var result = db.close();
-			result.open.should.be.false;
-	  });
+		var count = 1000;
+
+		it('should run serial', function(done) {
+			db.serialize(function() {
+				db.run('CREATE TABLE foo (txt text, num int, flt float, blb blob)');
+
+				var stmt = db.prepare('INSERT INTO foo VALUES(?, ?, ?, ?)');
+				for (var i = 0; i < count; i++) {
+					stmt.run('String ' + i, i, i * Math.PI, function(err) {
+						if (err) throw err;
+						inserted++;
+					});
+				}
+				stmt.finalize();
+
+				db.all('SELECT txt, num, flt, blb FROM foo ORDER BY num', function(err, rows) {
+					if (err) throw err;
+					for (var i = 0; i < rows.length; i++) {
+						rows[i].txt.should.eql('String ' + i);
+						rows[i].num.should.eql(i);
+						rows[i].flt.should.eql(i * Math.PI);
+						assert.equal(rows[i].blb, null);
+						retrieved++;
+					}
+					done();
+				});
+			});
+		});
+
+
+		it('should have inserted and retrieved all rows', function() {
+			count.should.eql(inserted);
+			count.should.eql(retrieved);
+		});
+
+		after(function(done) {
+			db.close(done);
+		});
 	});
 });
