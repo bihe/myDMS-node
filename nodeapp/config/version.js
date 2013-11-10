fca5907140f6cc69d4374105960be41f165f7ab1
Henrik Binggl <henrik@binggl.net>
2013-11-10 14:23:05 +0100

    Add startup scripts and version information

--- nodeapp/config/version.js
+++ nodeapp/config/version.js
@@ -1,4 +1,4 @@
 var version = {};
-version.number = 'v1.0.0';
+version.number = 'v1.0.20';
 version.api = '1.0';
 module.exports = version;
---
+++ scripts/version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=`/usr/local/bin/git-revision.sh`
+VERSION_FILE='./nodeapp/config/version.js'
+
+echo "var version = {};" > $VERSION_FILE
+echo "version.number = '$VERSION';" >> $VERSION_FILE
+echo "version.api = '1.0';" >> $VERSION_FILE
+echo "module.exports = version;" >> $VERSION_FILE
